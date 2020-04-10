2607e74637e807bdd1111482f54696dc050a11fe
Martin Hudik <hudikm@gmail.com>
2020-04-10 16:05:15 +0200

    1.3 Vytvorenie pomocnej metódy prepareHandler

--- app/src/main/java/sk/uniza/handlerthreadcvicenie/MyHandlerThread.java
+++ app/src/main/java/sk/uniza/handlerthreadcvicenie/MyHandlerThread.java
@@ -1,14 +1,22 @@
 package sk.uniza.handlerthreadcvicenie;
 
 import android.graphics.Bitmap;
+import android.os.Handler;
 import android.os.HandlerThread;
-import android.os.Process;
+import android.os.Message;
 import android.widget.ImageView;
+import android.os.Process;
+
+import androidx.annotation.NonNull;
 
 public class MyHandlerThread extends HandlerThread {
 
     // Názov vlákna, ktorý je zobrazený pri debugovaní aplikácie
     private static final String TAG = MyHandlerThread.class.getSimpleName();
+
+    // Definovanie pracovného handlera, ktorý z prideleného loopera príjima
+    // pridelenú prácu
+    private Handler mWorkerHandler;
 
     // Rozhranie pomocou ktorého sa odovzdá stiahnutý obrázok
     public interface Callback {
@@ -21,4 +29,21 @@ public class MyHandlerThread extends HandlerThread {
         // priority pracovnému vláknu
 
     }
+
+    /**
+     * Pomocná metóda na inicializáciu HandlerThread ako pracovného vlákna
+     */
+    public void prepareHandler() {
+        mWorkerHandler = new Handler(getLooper()) {
+            /*
+            V handleMessage callback metóde sa spracovávajú prijaté spravy na
+             pracovnom vlákne HandlerThread
+             */
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                // Spracovanie prijatej správy
+            }
+        };
+    }
+
 }
