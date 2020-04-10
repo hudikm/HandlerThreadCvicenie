bbf884bc75f66ed597f98c2985c5f1fc5b7dc525
Martin Hudik <hudikm@gmail.com>
2020-04-10 16:22:36 +0200

    1.5 Vytvorenie metódy handleRequest určenej na stiahnutie obrázku z internetu na pracovnom vlákne

--- app/src/main/java/sk/uniza/handlerthreadcvicenie/MyHandlerThread.java
+++ app/src/main/java/sk/uniza/handlerthreadcvicenie/MyHandlerThread.java
@@ -1,13 +1,20 @@
 package sk.uniza.handlerthreadcvicenie;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
+import android.os.Trace;
 import android.widget.ImageView;
 import android.os.Process;
 
 import androidx.annotation.NonNull;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MyHandlerThread extends HandlerThread {
 
@@ -42,9 +49,53 @@ public class MyHandlerThread extends HandlerThread {
             @Override
             public void handleMessage(@NonNull Message msg) {
                 // Spracovanie prijatej správy
+
             }
         };
     }
 
+    /*
+        Metóda na stiahnutie obrázku z internetu, Táto metóda je je spustená
+        na pracovnom vlákne!
+     */
+    private void handleRequest(final ImageUrl imageUrl) {
+
+        try {
+            final Bitmap bitmap;
+
+            Trace.beginSection("HTTP download");
+            HttpURLConnection connection =
+                    (HttpURLConnection) new URL(imageUrl.urlOfImage)
+                            .openConnection();
+            connection.setRequestMethod("GET");
+            bitmap = BitmapFactory
+                    .decodeStream((InputStream) connection.getContent());
+
+            try {
+                this.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            /*
+                Stiahnutý obrázok sa odovzdá s pomocu Handler triedy UI
+                vláknu, ktoré zavolá callback metódu
+             */
+            if (bitmap != null) {
+                mResponseHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Spustené na UI vlákne
+                        mCallback.onImageDownloaded(
+                                imageUrl.uiToShowImage.get(),
+                                bitmap);
+                    }
+                });
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
