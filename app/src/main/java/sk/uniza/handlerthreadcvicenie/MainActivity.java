a98f5e44094b567d9ee8996bb09f7157b0ffa5b5
Martin Hudik <hudikm@gmail.com>
2020-04-10 16:52:58 +0200

    1.8 Vytvorenie a inicializovanie novej inštancie MyHandlerThread

--- app/src/main/java/sk/uniza/handlerthreadcvicenie/MainActivity.java
+++ app/src/main/java/sk/uniza/handlerthreadcvicenie/MainActivity.java
@@ -3,17 +3,26 @@ package sk.uniza.handlerthreadcvicenie;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
         implements MyHandlerThread.Callback {
 
+    private LinearLayout imageContainer;
+    private MyHandlerThread myHandlerThread;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myHandlerThread = new MyHandlerThread(new Handler(), this);
+
+        myHandlerThread.start();
+        myHandlerThread.prepareHandler();
     }
 
     @Override
