f512c91c96472a26879f73d17e06ad4d1c0e8b94
Martin Hudik <hudikm@gmail.com>
2020-04-11 14:32:07 +0200

    1.2.b Použitie rozhrania MyHandlerThread.Callback

--- app/src/main/java/sk/uniza/handlerthreadcvicenie/MainActivity.java
+++ app/src/main/java/sk/uniza/handlerthreadcvicenie/MainActivity.java
@@ -1,14 +1,23 @@
+
 package sk.uniza.handlerthreadcvicenie;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MyHandlerThread.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onImageDownloaded(ImageView imageView, Bitmap bitmap) {
+
     }
 }
