4b4a0f9a37a5ecbbc6197dbf6b74cbef1080fd07
Martin Hudik <hudikm@gmail.com>
2020-04-11 14:31:39 +0200

    1.2.a Vytvorenie rozhrania, ktoré sa použije na odovzdanie stiahnutého obrázku

--- app/src/main/java/sk/uniza/handlerthreadcvicenie/MyHandlerThread.java
+++ app/src/main/java/sk/uniza/handlerthreadcvicenie/MyHandlerThread.java
@@ -1,12 +1,19 @@
 package sk.uniza.handlerthreadcvicenie;
 
+import android.graphics.Bitmap;
 import android.os.HandlerThread;
 import android.os.Process;
+import android.widget.ImageView;
 
 public class MyHandlerThread extends HandlerThread {
 
     // Názov vlákna, ktorý je zobrazený pri debugovaní aplikácie
     private static final String TAG = MyHandlerThread.class.getSimpleName();
+
+    // Rozhranie pomocou ktorého sa odovzdá stiahnutý obrázok
+    public interface Callback {
+        public void onImageDownloaded(ImageView imageView, Bitmap bitmap);
+    }
 
     public MyHandlerThread() {
         super(TAG);
